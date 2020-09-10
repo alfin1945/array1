33c394b7c2749746651ab17856f9db690b500049
alfin1945 <alfinmardani7@gmail.com>
2020-09-10 22:21:39 +0700

    soal1

---
+++ array1/tugas1.js
@@ -0,0 +1,6 @@
+var desaBantul = [
+    ['argodadi', 'argomulyo', 'argorejo', 'argosari'],
+    ['bangunjiwo', 'banguntapan', 'bantul', 'baturetno', 'bawuran'],
+    ['canden', 'caturharjo', 'cetan Srigading']
+]
+console.log(desaBantul)
